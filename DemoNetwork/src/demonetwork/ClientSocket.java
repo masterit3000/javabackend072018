21d3e92219fe6f0825f87d5ba76e08ce90d44856
Dang Tuan Tu Tu <masterit3000@gmail.com>
2018-08-24 21:06:39 +0700

    demosocket 2

--- DemoNetwork/src/demonetwork/ClientSocket.java
+++ DemoNetwork/src/demonetwork/ClientSocket.java
@@ -5,9 +5,12 @@
  */
 package demonetwork;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -16,6 +19,8 @@ import java.util.logging.Logger;
 /**
  *
  * @author Admin
+ * Lap trinh  doc 1 file trong may va gui sang may khac
+ * Lap trình 1 server, nhan duoc nhieu client 
  */
 public class ClientSocket {
 
@@ -25,15 +30,41 @@ public class ClientSocket {
             Socket socket = new Socket("localhost", 4567);
             System.out.println("Ket noi thanh cong toi server: " + socket.getPort());
             System.out.println("Ket noi thanh cong toi server: " + socket.getInetAddress().getHostName());
-
+            InetAddress inetAddress = socket.getInetAddress();
+//            inetAddress.
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            Thread th = new Thread(new Runnable() {
+                @Override
+
+                public void run() {
+                    while (true) {
+                        try {
+                            String readLine = reader.readLine();
+                            if (readLine == null) {
+                                break;
+                            }
+                            System.out.println("Server: " + readLine);
+                        } catch (IOException ex) {
+                            System.out.println("Gap su co ket noi: ");
+                            break;
+                        }
+                    }
+                }
+            });
+            th.start();
             while (true) {
+                if (socket.isClosed()) {
+                    break;
+                }
                 String s = new Scanner(System.in).nextLine();
                 writer.write(s);
                 writer.newLine();
                 writer.flush();
             }
 
+            socket.close();//luu y dong trong finaly
         } catch (IOException ex) {
             Logger.getLogger(ClientSocket.class.getName()).log(Level.SEVERE, null, ex);
         }
--- DemoNetwork/src/demonetwork/Server.java
+++ DemoNetwork/src/demonetwork/Server.java
@@ -6,11 +6,14 @@
 package demonetwork;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -33,13 +36,34 @@ public class Server {
             System.out.println("Client ket noi thanh cong: " + socket.getPort());
 //            InputStream inputStream = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            Thread th = new Thread(new Runnable() {
+                @Override
+
+                public void run() {
+
+                    while (true) {
+                        try {
+                            String readLine = reader.readLine();
+                            if (readLine == null) {
+                                break;
+                            }
+                            System.out.println("Client: " + readLine);
+                        } catch (IOException ex) {
+                            System.out.println("Gap su co ket noi: ");
+                            break;
+                        }
+                    }
+                }
+            });
+            th.start();
 
             while (true) {
-                String readLine = reader.readLine();
-                if (readLine == null) {
-                    break;
-                }
-                System.out.println("Client: " + readLine);
+                String s = new Scanner(System.in).nextLine();
+                writer.write(s);
+                writer.newLine();
+                writer.flush();
             }
         } catch (IOException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
